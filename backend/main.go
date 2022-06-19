1873a63250adc207622720aaf8d433bc1280e60e
snorkilator <daniel@rudenberg.ca>
2022-06-19 12:33:45 -0400

    MADE REACT FORM WORK!! Got rid of junk files, updated server blueprint.

--- backend/main.go
+++ backend/main.go
@@ -3,49 +3,77 @@ package main
 
 import (
 	_ "database/sql"
-	_ "fmt"
+	"encoding/json"
+	"fmt"
 	_ "net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 //starts server
 func main() {
-	/*connect to db
-	  get db connection
-	  register universal handler
-	  		receives JSON requests
-	  		unmarhshals
-	  		switch
-	  			depending on action:
-	  			switches to one of the following
-	  			hands it the unmarshalled struct
-	  				new
-	  					checks if it already exists
-	  					if it does, for now throw error (overwrite? prompt back to client)
-	  				delete
-	  					checks if it already exists
-	  					if it doesn't, throw error and delete confirmaion
-	  					if it does
-	  						tell db to delete page
-	  						after no error,
-	  							send confirmation to client
+	conf := getConf()
+	fmt.Print(conf)
+
+	/*
+		read from config file
+		connect to db
+		get db connection
+		register universal handler
+			receives JSON requests
+			unmarhshals
+			switch
+				depending on action:
+				switches to one of the following
+				hands it the unmarshalled struct
+					new
+						validate the date
+						checks if it already exists
+						if it does, for now throw error (overwrite? prompt back to client)
+					delete
+						checks if it already exists
+						if it doesn't, throw error and delete confirmaion
+						if it does
+							tell db to delete page
+							after no error,
+								send confirmation to client
 							after error,
 								send did not delete response back
-	  				edit
-	  					write it to database
-	  					if error, send could not save response
-	  					no error, send edit confirmation to client
-	  				all pages
-	  					send query of hours
+					edit
+						validate the date
+						write it to database
+						if error, send could not save response
+						no error, send edit confirmation to client
+					all pages
+						send query of hours
 						if there are > 0 pages
 							put into array, marhsall, send as json
-	  				page
+					page
 						send QueryRow
 						if received,
 							marshall and send json
 						not received
 							send error, page does not exist
-	  start handle and serve
+		start handle and serve
 	*/
+}
+
+type config struct {
+	Database struct {
+		Ip       string `json:"ip"`
+		Port     string `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	} `json:"database"`
+}
+
+func getConf() config {
+	var conf = new(config)
+	f, _ := os.ReadFile("./config.json")
+	fmt.Println(f)
+	if err := json.Unmarshal(f, &conf); err != nil {
+		fmt.Print(err)
+	}
+	return *conf
 }
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { isPropertySignature } from "typescript";
 import "./App.css";
 
 export default function App() {
@@ -29,27 +30,77 @@ function Home() {
   );
 }
 
-function DayView() {
-  return (
-    <>
-      <main>
-        <h2>July 1st</h2>
-        <HowWasYourDayForm />
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
+type Props = {};
+
+type state = {
+  isToggleOn: boolean;
+  howWasYourDay: string;
+};
+
+class DayView extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <>
+        <main>
+          <h2>July 1st</h2>
+          <Toggle />
+          <HowWasYourDayForm />
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
+}
+
+class Toggle extends React.Component {
+  state: state // creates state property with type state
+  handleClick() {    this.setState((prevState: {isToggleOn:boolean}) => ({      isToggleOn: !prevState.isToggleOn    }));  };
+  constructor(props: any) { // when new Toggle(arugments) is called, this is run
+    super(props); // passes props to component constructo
+    // sets state property to initial state
+    // prevState: enherited property??
+    this.state = {isToggleOn: true, howWasYourDay: ""};
+    // This binding is necessary to make `this` work in the callback 
+    //method bind() receives function, and take this as parameter
+    //    makes it so, in the functions context, 'this' still refers to class object
+    this.handleClick = this.handleClick.bind(this);  }
+
+  
+  render() {
+    return (
+      <button onClick={this.handleClick}>        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
 }
 
 class HowWasYourDayForm extends React.Component {
+  state: {value: string}
+  constructor(props: any){
+    super(props)
+    this.state = {value: ""}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event: any) {
+    this.setState({value: event.target.value})
+  }
+  
+  handleSubmit(event: any){
+    alert('submitted' + this.state.value);
+    event.preventDefault();
+  }
+  
   render() {
     return (
-      <form onSubmit={() => alert("submit")}>
+      <form onSubmit={this.handleSubmit}>
         <label>What did you work on today?</label>
         <br />
-        <textarea />
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
         <br />
         <input type="submit" value="Submit" />
       </form>
