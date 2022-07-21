d74703c3b62f35b52134d7887477c7ba8931fa3f
snorkilator <daniel@rudenberg.ca>
2022-07-21 14:15:08 -0400

    state refactor

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -35,7 +35,7 @@ constructor(props: any){
   let date = new Date()
   let p = [{table: [{id: 0, hrs: 0, activity: ""}], goals: "", date: new Date()}]as never;
   this.state = { pages: p };  
-
+  this.setState = this.setState.bind(this)
   fetch("/getall").then((data) => {
     data.json().then((data) => {
       this.setState({ pages: data.data });
@@ -46,9 +46,10 @@ constructor(props: any){
   return (
     <div className="App">
       <h1>Hour Logger</h1>
+      {this.state.pages[0].goals}
       <Routes>
-        <Route path="/" element={<Home pages={this.state.pages} />} />
-        <Route path="/dayview/" element={<DayView pages={this.state.pages} />} />
+        <Route path="/" element={<Home pages={this.state.pages}  />} />
+        <Route path="/dayview/" element={<DayView pages={this.state.pages} setState={this.setState} />} />
       </Routes>
     </div>
   );
